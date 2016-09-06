525d775a471515963966cf4fd69bdd006d86caf5
Cyrille COLLIN <Cyrille COLLIN>
2016-09-06 14:44:20 +0200

    git commit -am "Ajout de la fonction checkAnniv()"

--- scripts.js
+++ scripts.js
@@ -21,3 +21,20 @@ function afficheDate(){
     var moisToday = mois[laDate.getMonth()]; 
     document.write("Ajourd'hui, nous sommes le : "+ jourToday+" "+ moisToday);
 }
+
+function checkAnniv(){
+// var datesCollegues = new Array();
+// datesCollegues[0] = "Février";
+// datesCollegues[1] = "Septembre";
+
+// Récupération du jour et du mois courant
+// GetMonth étant numéroté de 0 à 11, ajout de +1 à GetMonth pour avoir le véritable numéro de mois
+var laDate = new Date();
+var jourToday = laDate.getDate();
+var moisToday = (laDate.getMonth()+1);    
+
+ document.write("Ajourd'hui, nous sommes le : "+ jourToday+" "+ moisToday);
+   
+
+
+}
