d7cf12e36ac655dd5233c127772b1aed13afbe7f
Cyrille COLLIN <Cyrille COLLIN>
2016-09-06 16:17:16 +0200

    commit -am "Ajout vérification anniversaires"

--- scripts.js
+++ scripts.js
@@ -2,6 +2,53 @@
 PREMIER SCRIPT : ANNIVERSAIRE
 */
 
+function checkAnniv() {
+// Récupération du jour et du mois courant
+// GetMonth étant numéroté de 0 à 11, ajout de +1 à GetMonth pour avoir le véritable numéro de mois
+var laDate = new Date();
+var jourToday = laDate.getDate();
+var moisToday = (laDate.getMonth()+1);    
+
+// création de l'objet collegue
+// liste des collègues
+var simon=new collegue ("Simon","4","9");
+var cyrille=new collegue ("Cyrille","3","2");
+var martine=new collegue ("Martine","12","9");
+
+// création du tableau collegues
+        var collegues = new Array;
+        collegues[0]=simon;
+        collegues[1]=cyrille;
+        collegues[2]=martine;
+        }
+
+        function collegue(un_prenom, un_jour_naissance, un_mois_naissance){
+            this.prenom=un_prenom;
+            this.jourNaissance=un_jour_naissance;
+            this.moisNaissance=un_mois_naissance;
+        }
+        for (i=0; i<= collegues.length;i++){
+   
+        if (moisToday==collegues[i].moisNaissance && jourToday==collegues[i].jourNaissance){
+        document.write("Joyeux anniversaire "+ collegues[i].prenom+" !  "+"<br/> ");
+       
+        
+        }
+        else if (moisToday==collegues[i].moisNaissance && jourToday>collegues[i].jourNaissance){
+    
+        document.write("Ce mois-ci, c'était l'anniversaire de : "+ collegues[i].prenom+" le  "+collegues[i].jourNaissance+"/"+collegues[i].moisNaissance+"<br/> ");
+      
+       
+        }
+        else if (moisToday==collegues[i].moisNaissance && jourToday<collegues[i].jourNaissance){
+     
+         document.write("Ce mois-ci, c'est l'anniversaire de : "+ collegues[i].prenom+" le  "+collegues[i].jourNaissance+"/"+collegues[i].moisNaissance+"<br/> ");
+    
+       
+        }
+        document.write("test2");
+}
+
 function afficheDate(){
     var laDate = new Date();
     var jourToday = laDate.getDate();
@@ -20,21 +67,4 @@ function afficheDate(){
     mois[11] = "Décembre";
     var moisToday = mois[laDate.getMonth()]; 
     document.write("Ajourd'hui, nous sommes le : "+ jourToday+" "+ moisToday);
-}
-
-function checkAnniv(){
-// var datesCollegues = new Array();
-// datesCollegues[0] = "Février";
-// datesCollegues[1] = "Septembre";
-
-// Récupération du jour et du mois courant
-// GetMonth étant numéroté de 0 à 11, ajout de +1 à GetMonth pour avoir le véritable numéro de mois
-var laDate = new Date();
-var jourToday = laDate.getDate();
-var moisToday = (laDate.getMonth()+1);    
-
- document.write("Ajourd'hui, nous sommes le : "+ jourToday+" "+ moisToday);
-   
-
-
 }
